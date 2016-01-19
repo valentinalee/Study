7741884a58341d79907baeb4cfcadb88f9b6994d
tiny_lk <tiny.lk@gmail.com>
2016-01-19 21:28:31 +0800

    add handlebars

--- 01.meadowlark/index.js
+++ 01.meadowlark/index.js
@@ -1,19 +1,31 @@
 var express = require('express');
 var app = express();
 app.set('port', process.env.PORT || 3000);
-// 定制404 页面
-app.use(function(req, res){
-    res.type('text/plain');
-    res.status(404);
-    res.send('404 - Not Found');
+
+// 设置handlebars 视图引擎
+var handlebars = require('express-handlebars')
+.create({ defaultLayout:'main' });
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.get('/', function(req, res) {
+    res.render('home');
 });
-// 定制500 页面
+app.get('/about', function(req, res) {
+    res.render('about');
+});
+// 404 catch-all 处理器（中间件）
+app.use(function(req, res, next){
+    res.status(404);
+    res.render('404');
+});
+// 500 错误处理器（中间件）
 app.use(function(err, req, res, next){
     console.error(err.stack);
-    res.type('text/plain');
     res.status(500);
-    res.send('500 - Server Error');
+    res.render('500');
 });
+
 app.listen(app.get('port'), function(){
     console.log( 'Express started on http://localhost:' +
     app.get('port') + '; press Ctrl-C to terminate.' );
