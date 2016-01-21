c3e6c2a470a49a22fe866e1addb16d5b643ae041
tiny_lk <tiny.lk@gmail.com>
2016-01-21 08:21:44 +0800

    node-and-express ch03

--- 01.meadowlark/index.js
+++ 01.meadowlark/index.js
@@ -2,17 +2,29 @@ var express = require('express');
 var app = express();
 app.set('port', process.env.PORT || 3000);
 
+var fortunes = [
+"Conquer your fears or they will conquer you.",
+"Rivers need springs.",
+"Do not fear what you don't know.",
+"You will have a pleasant surprise.",
+"Whenever possible, keep it simple.",
+];
+
 // 设置handlebars 视图引擎
-var handlebars = require('express-handlebars')
-.create({ defaultLayout:'main' });
+var handlebars = require('express-handlebars').create({ defaultLayout:'main' });
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
+
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
     res.render('home');
 });
+
 app.get('/about', function(req, res) {
-    res.render('about');
+    var randomFortune =
+    fortunes[Math.floor(Math.random() * fortunes.length)];
+    res.render('about', { fortune: randomFortune });
 });
 // 404 catch-all 处理器（中间件）
 app.use(function(req, res, next){
