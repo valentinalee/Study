12ad98b2d1d75958be907892209796ae07922dad
likun <likun2@cjwsjy.com.cn>
2016-01-30 15:31:10 +0800

    node-and-express ch08

--- 01.meadowlark/index.js
+++ 01.meadowlark/index.js
@@ -1,4 +1,7 @@
 var express = require('express');
+var bodyParser  = require('body-parser');
+var formidable = require('formidable');
+
 var app = express();
 app.set('port', process.env.PORT || 3000);
 
@@ -21,6 +24,10 @@ app.set('view engine', 'handlebars');
 app.disable('x-powered-by');
 
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.use(function(req, res, next){
     res.locals.showTests = app.get('env') !== 'production' &&
@@ -101,6 +108,42 @@ app.get('/data/nursery-rhyme', function(req, res){
 		adjective: 'bushy',
 		noun: 'heck',
 	});
+});
+app.get('/thank-you', function(req, res){
+	res.render('thank-you');
+});
+app.get('/newsletter', function(req, res){
+    // we will learn about CSRF later...for now, we just
+    // provide a dummy value
+    res.render('newsletter', { csrf: 'CSRF token goes here' });
+});
+app.post('/process', function(req, res){
+    console.log('Form (from querystring): ' + req.query.form);
+    console.log('CSRF token (from hidden form field): ' + req.body._csrf);
+    console.log('Name (from visible form field): ' + req.body.name);
+    console.log('Email (from visible form field): ' + req.body.email);
+    if(req.xhr || req.accepts('json,html')==='json'){
+        // if there were an error, we would send { error: 'error description' }
+        res.send({ success: true });
+    } else {
+        // if there were an error, we would redirect to an error page
+        res.redirect(303, '/thank-you');
+    }
+});
+app.get('/contest/vacation-photo', function(req, res){
+    var now = new Date();
+    res.render('contest/vacation-photo', { year: now.getFullYear(), month: now.getMonth() });
+});
+app.post('/contest/vacation-photo/:year/:month', function(req, res){
+    var form = new formidable.IncomingForm();
+    form.parse(req, function(err, fields, files){
+        if(err) return res.redirect(303, '/error');
+        console.log('received fields:');
+        console.log(fields);
+        console.log('received files:');
+        console.log(files);
+        res.redirect(303, '/thank-you');
+    });
 });
 
 app.get('/headers', function(req,res){
