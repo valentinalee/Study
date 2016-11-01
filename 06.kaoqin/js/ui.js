e3fec9a678a9ab029ce4692142220a01332641f7
tiny.lk <tiny.lk@gmail.com>
2016-11-01 21:12:51 +0800

    kaoqin electron ui

--- 06.kaoqin/js/ui.js
+++ 06.kaoqin/js/ui.js
@@ -1,17 +1,57 @@
 const ipc = require('electron').ipcRenderer
 
-const btnSelectSL = document.getElementById('btnSelectSL')
+const btnSelectSL = document.getElementById('btnSelectSL');
 btnSelectSL.addEventListener('click', function (event) {
-    ipc.send('open-sl-file-dialog')
+    ipc.send('open-sl-file-dialog');
 })
-ipc.on('selected-sl-file', function (event, path) {
-    document.getElementById('txtSL').value = path;
+ipc.on('selected-sl-file', function (event, data) {
+    document.getElementById('txtSL').value = data.file;
+    if(data.sheets){
+        let selSL  = document.getElementById('selSL');
+        selSL.options.length = 0;
+        for(let i=0;i < data.sheets.length; i++){
+            selSL.options.add(new Option(data.sheets[i],data.sheets[i]));
+        }
+    }
 })
 
-const btnSelectSK = document.getElementById('btnSelectSK')
+const btnSelectSK = document.getElementById('btnSelectSK');
 btnSelectSK.addEventListener('click', function (event) {
-    ipc.send('open-sk-file-dialog')
+    ipc.send('open-sk-file-dialog');
 })
-ipc.on('selected-sk-file', function (event, path) {
-    document.getElementById('txtSK').value = path;
+ipc.on('selected-sk-file', function (event, data) {
+    document.getElementById('txtSK').value = data.file;
+    if(data.sheets){
+        let selSK  = document.getElementById('selSK');
+        selSK.options.length = 0;
+        for(let i=0;i < data.sheets.length; i++){
+            selSK.options.add(new Option(data.sheets[i],data.sheets[i]));
+        }
+    }
+})
+
+const btnSelectOut = document.getElementById('btnSelectOut');
+btnSelectOut.addEventListener('click', function (event) {
+    ipc.send('open-out-file-dialog');
+})
+ipc.on('selected-out-file', function (event, path) {
+    document.getElementById('txtOut').value = path;
+})
+
+const btnOK = document.getElementById('btnOK');
+btnOK.addEventListener('click', function (event) {
+    let selSL  = document.getElementById('selSL');
+    let selSK  = document.getElementById('selSK');
+    ipc.send('save-file',{
+        slSheet:selSL.options[selSL.selectedIndex].value,
+        skSheet:selSK.options[selSK.selectedIndex].value,
+        outFile:document.getElementById('txtOut').value
+    });
+})
+ipc.on('saved-file-ok', function (event) {
+    alert('成功！');
+})
+
+ipc.on('err-msg', function (event, err) {
+    alert(err);
 })
--- 06.kaoqin/main.js
+++ 06.kaoqin/main.js
@@ -1,6 +1,14 @@
-const {app, BrowserWindow} = require('electron')
-const ipc = require('electron').ipcMain
-const dialog = require('electron').dialog
+const {app, BrowserWindow} = require('electron');
+const ipc = require('electron').ipcMain;
+const dialog = require('electron').dialog;
+const _ = require('lodash');
+const XLSX = require('xlsx');
+const kaoqin = require('./js/kaoqin');
+
+//刷卡记录excel
+let skWorkbook = null;
+//刷脸记录excel
+let slWorkbook = null;
 
 let win
 
@@ -35,14 +43,25 @@ app.on('activate', () => {
     }
 })
 
+function sendError(sender,err){
+    sender.send('err-msg',err.message);
+}
+
 ipc.on('open-sl-file-dialog', function (event) {
     dialog.showOpenDialog({
         properties: ['openFile'],
         filters: [
-            { name: 'Excel', extensions: ['xls', 'xlsx'] },
+            { name: 'Excel(*.xls,*.xlsx)', extensions: ['xls', 'xlsx'] },
         ]
     }, function (files) {
-        if (files) event.sender.send('selected-sl-file', files)
+        if (files && files.length > 0){
+            try {
+                slWorkbook = XLSX.readFile(files[0]);
+                event.sender.send('selected-sl-file', {file:files[0],sheets:slWorkbook.SheetNames});
+            } catch (err) {
+                sendError(event.sender,err);
+            }
+        } 
     })
 })
 
@@ -50,9 +69,41 @@ ipc.on('open-sk-file-dialog', function (event) {
     dialog.showOpenDialog({
         properties: ['openFile'],
         filters: [
-            { name: 'Excel', extensions: ['xls', 'xlsx'] },
+            { name: 'Excel(*.xls,*.xlsx)', extensions: ['xls', 'xlsx'] },
         ]
     }, function (files) {
-        if (files) event.sender.send('selected-sk-file', files)
+        if (files && files.length > 0){
+            try {
+                skWorkbook = XLSX.readFile(files[0]);
+                event.sender.send('selected-sk-file', {file:files[0],sheets:skWorkbook.SheetNames});
+            } catch (err) {
+                sendError(event.sender,err);
+            }
+        } 
     })
+})
+
+ipc.on('open-out-file-dialog', function (event) {
+    dialog.showSaveDialog({
+        title: '保存结果',
+        filters: [
+            { name: 'Excel(*.xlsx)', extensions: ['xlsx'] },
+        ]
+    }, function (filename) {
+        if (filename) event.sender.send('selected-out-file', filename)
+    })
+})
+
+ipc.on('save-file', function (event,data) {
+    try {
+        //刷卡记录sheet
+        const skSheet = skWorkbook.Sheets[data.skSheet];
+        //刷脸记录sheet
+        const slSheet = slWorkbook.Sheets[data.slSheet];
+        let out_workbook = kaoqin.countKaoQin(slSheet,skSheet);
+        XLSX.writeFile(out_workbook, data.outFile);
+        event.sender.send('saved-file-ok', 'ok');
+    } catch (err) {
+        sendError(event.sender,err);
+    }
 })
