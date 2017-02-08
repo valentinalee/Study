024c7a7609d77e343b6e23146b46986c67c73d0b
lik <tiny.lk@gmail.com>
2017-02-08 17:19:13 +0800

    SpringBootDemo chapter3-1-1

--- 08.SpringBootDemo/src/test/java/cn/chinaunicom/SpringBootDemoApplicationTests.java
+++ 08.SpringBootDemo/src/test/java/cn/chinaunicom/SpringBootDemoApplicationTests.java
@@ -1,6 +1,7 @@
 package cn.chinaunicom;
 
 import cn.chinaunicom.web.HelloController;
+import cn.chinaunicom.web.UserController;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,10 +11,13 @@ import org.springframework.mock.web.MockServletContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -25,7 +29,7 @@ public class SpringBootDemoApplicationTests {
 
 	@Before
 	public void setUp() throws Exception {
-		mvc = MockMvcBuilders.standaloneSetup(new HelloController()).build();
+		mvc = MockMvcBuilders.standaloneSetup(new HelloController(),new UserController()).build();
 	}
 
 	@Test
@@ -33,6 +37,56 @@ public class SpringBootDemoApplicationTests {
 		mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk())
 				.andExpect(content().string(equalTo("Hello World")));
+	}
+
+	@Test
+	public void testUserController() throws Exception {
+		// 测试UserController
+		RequestBuilder request = null;
+
+		// 1、get查一下user列表，应该为空
+		request = get("/users/");
+		mvc.perform(request)
+				.andExpect(status().isOk())
+				.andExpect(content().string(equalTo("[]")));
+
+		// 2、post提交一个user
+		request = post("/users/")
+				.param("id", "1")
+				.param("name", "测试大师")
+				.param("age", "20");
+		mvc.perform(request)
+				.andExpect(content().string(equalTo("success")));
+
+		// 3、get获取user列表，应该有刚才插入的数据
+		request = get("/users/");
+		mvc.perform(request)
+				.andExpect(status().isOk())
+				.andExpect(content().string(equalTo("[{\"id\":1,\"name\":\"测试大师\",\"age\":20}]")));
+
+		// 4、put修改id为1的user
+		request = put("/users/1")
+				.param("name", "测试终极大师")
+				.param("age", "30");
+		mvc.perform(request)
+				.andExpect(content().string(equalTo("success")));
+
+		// 5、get一个id为1的user
+		request = get("/users/1");
+		mvc.perform(request)
+				.andExpect(content().string(equalTo("{\"id\":1,\"name\":\"测试终极大师\",\"age\":30}")));
+
+		// 6、del删除id为1的user
+		request = delete("/users/1");
+		mvc.perform(request)
+				.andExpect(content().string(equalTo("success")));
+
+		// 7、get查一下user列表，应该为空
+		request = get("/users/");
+		mvc.perform(request)
+				.andExpect(status().isOk())
+				.andExpect(content().string(equalTo("[]")));
+
 	}
 
 }
