243d1c9c9b2ef1e32a9437abad956c2a1f67c4bc
lik <tiny.lk@gmail.com>
2017-02-08 13:11:21 +0800

    add SpringBootDemo

---
+++ 08.SprintBootDemo/src/main/java/cn/chinaunicom/SprintBootDemoApplication.java
@@ -0,0 +1,12 @@
+package cn.chinaunicom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintBootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintBootDemoApplication.class, args);
+	}
+}
