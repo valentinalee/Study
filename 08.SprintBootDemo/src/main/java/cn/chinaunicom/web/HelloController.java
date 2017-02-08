286e47140927905d79b02627807d35ed182750b2
lik <tiny.lk@gmail.com>
2017-02-08 14:43:05 +0800

    chapter1

---
+++ 08.SprintBootDemo/src/main/java/cn/chinaunicom/web/HelloController.java
@@ -0,0 +1,15 @@
+package cn.chinaunicom.web;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String index() {
+        return "Hello World";
+    }
+
+}
