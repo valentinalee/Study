b3a6cae49f8d64e53bd6255b5e4c7f95711db5e9
lik <tiny.lk@gmail.com>
2017-02-09 15:32:28 +0800

    SpringBootDemo chapter3-1-6

---
+++ 08.SpringBootDemo/src/main/java/cn/chinaunicom/exception/MyException.java
@@ -0,0 +1,8 @@
+package cn.chinaunicom.exception;
+
+public class MyException extends Exception {
+    public MyException(String message) {
+        super(message);
+    }
+
+}
--- 08.SpringBootDemo/src/main/java/cn/chinaunicom/web/HelloController.java
+++ 08.SpringBootDemo/src/main/java/cn/chinaunicom/web/HelloController.java
@@ -1,6 +1,7 @@
 package cn.chinaunicom.web;
 
 
+import cn.chinaunicom.exception.MyException;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,11 @@ public class HelloController {
     @RequestMapping("/hello")
     public String index() {
         return "Hello World";
+    }
+
+    @RequestMapping("/json")
+    public String json() throws MyException {
+        throw new MyException("发生错误2");
     }
 
 }
