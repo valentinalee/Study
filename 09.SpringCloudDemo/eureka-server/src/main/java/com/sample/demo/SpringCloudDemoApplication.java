747a747baa46c3dc5c1fb458c1eff77adbf3c1f0
lik <tiny.lk@gmail.com>
2017-02-14 15:07:43 +0800

    SpringCloudDemo chapter1-1-1

---
+++ 09.SpringCloudDemo/eureka-server/src/main/java/com/sample/demo/SpringCloudDemoApplication.java
@@ -0,0 +1,15 @@
+package com.sample.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class SpringCloudDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudDemoApplication.class, args);
+	}
+}
