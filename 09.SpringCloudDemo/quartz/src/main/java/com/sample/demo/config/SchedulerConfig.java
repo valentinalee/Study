ea5b8402ab53f3d5e43e10b221b5eae158a396e2
lik <lik40@chinaunicom.cn>
2017-09-02 21:45:32 +0800

    edit 09 quartz

--- 09.SpringCloudDemo/quartz/src/main/java/com/sample/demo/config/SchedulerConfig.java
+++ 09.SpringCloudDemo/quartz/src/main/java/com/sample/demo/config/SchedulerConfig.java
@@ -1,14 +1,19 @@
 package com.sample.demo.config;
 
 import org.quartz.spi.JobFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.config.PropertiesFactoryBean;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 import org.springframework.transaction.PlatformTransactionManager;
-
 
 import javax.sql.DataSource;
 import java.io.IOException;
@@ -20,7 +25,11 @@ import java.util.Properties;
 @Configuration
 public class SchedulerConfig {
 
-    public static final String QUARTZ_PROPERTIES_PATH = "/quartz.properties";
+    @Value("${quartz.config}")
+    private String QUARTZ_PROPERTIES_PATH ;
+
+    @Autowired
+    private ResourceLoader resourceLoader;
 
     @Bean
     public JobFactory jobFactory(ApplicationContext applicationContext) {
@@ -30,14 +39,15 @@ public class SchedulerConfig {
     }
 
     @Bean
-    public SchedulerFactoryBean schedulerFactoryBean(JobFactory jobFactory, DataSource dataSource, PlatformTransactionManager transactionManager) throws IOException {
+    public SchedulerFactoryBean schedulerFactoryBean(JobFactory jobFactory, @Qualifier("quartz") DataSource dataSource,
+                                                     PlatformTransactionManager transactionManager) throws IOException {
         SchedulerFactoryBean factory = new SchedulerFactoryBean();
         factory.setAutoStartup(true);
         factory.setJobFactory(jobFactory);
         factory.setQuartzProperties(quartzProperties());
 
         //jdbc store
-//        factory.setDataSource(dataSource);
+        factory.setDataSource(dataSource);
         factory.setTransactionManager(transactionManager);
 
         return factory;
@@ -46,9 +56,22 @@ public class SchedulerConfig {
     @Bean
     public Properties quartzProperties() throws IOException {
         PropertiesFactoryBean propertiesFactoryBean = new PropertiesFactoryBean();
-        propertiesFactoryBean.setLocation(new ClassPathResource(QUARTZ_PROPERTIES_PATH));
+        propertiesFactoryBean.setLocation(resourceLoader.getResource(QUARTZ_PROPERTIES_PATH));
         propertiesFactoryBean.afterPropertiesSet();
         return propertiesFactoryBean.getObject();
     }
 
+
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix= "spring.datasource")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean(name= "quartz")
+    @ConfigurationProperties(prefix= "quartz.datasource")
+    public DataSource quartzDataSource() {
+        return DataSourceBuilder.create().build();
+    }
 }
