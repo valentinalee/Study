bc16806c50f561bb4a2543f75aa004c3efebdc38
lik <lik40@chinaunicom.cn>
2017-09-13 09:51:25 +0800

    add 13.node-weixin

---
+++ 13.node-weixin/app.js
@@ -0,0 +1,12 @@
+'use strict';
+/**
+ * Module dependencies.
+ */
+const koa = require('koa');
+const app = new koa();
+
+app.use(async ctx => {
+  ctx.body = 'Hello World';
+});
+
+app.listen(3000);
