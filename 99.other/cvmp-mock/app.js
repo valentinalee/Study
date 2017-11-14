a243dbdecc0432d1a8734bdf5216386319979900
lik <lik40@chinaunicom.cn>
2017-11-14 11:35:59 +0800

    edit cvmp-mock

--- 99.other/cvmp-mock/app.js
+++ 99.other/cvmp-mock/app.js
@@ -10,6 +10,7 @@ const _ = require('lodash');
 const uuidv4 = require('uuid/v4');
 const url = require("url");
 const validator = require('validator');
+const rp = require('request-promise-native');
 
 const port = 53080; //端口
 
@@ -23,7 +24,7 @@ const main = async ctx => {
 
 router.get('/', main)
 
-const authRequired = (ctx, next) => {
+const authRequired = async (ctx, next) => {
   let appId = ctx.header.app_key;
   let auth = ctx.header.authorization;
   let token = "";
@@ -34,7 +35,7 @@ const authRequired = (ctx, next) => {
   }
 
   if (appId == "test" && (token == "950a7cc9-5a8a-42c9-a693-40e817b1a4b0" || token == "56465b41-429d-436c-ad8d-613d476ff322")) {
-    next();
+    await next();
   } else {
     console.log(`app_key:${appId} auth:${auth} `);
     ctx.status = 401;
@@ -46,55 +47,105 @@ const authRequired = (ctx, next) => {
 
 }
 
-router.use('/api', authRequired);
+const authRequiredProxy = async (ctx, next) => {
+  let appId = ctx.header.app_key;
+  let auth = ctx.header.authorization;
+  let token = "";
 
-// 1.1.1 Authentication
-router.post('/iocm/app/sec/v1.1.0/login', async (ctx) => {
-  let appId = ctx.request.body.appId;
-  let secret = ctx.request.body.secret;
-  if ((appId == "test") && (secret == "test")) {
-    ctx.body = {
-      "accessToken": "950a7cc9-5a8a-42c9-a693-40e817b1a4b0",
-      "tokenType": "bearer",
-      "expiresIn": 7200,
-      "refreshToken": "773a0fcd-6023-45f8-8848-e141296cb3cb",
-      "scope": "default",
-    };
+  let authArray = _.split(auth, " ");
+  if (authArray.length >= 2) {
+    token = authArray[1];
+  }
+
+  if (appId && token) {
+    await next();
   } else {
+    console.log(`app_key:${appId} auth:${auth} `);
     ctx.status = 401;
     ctx.body = {
-      "error_code": 100208,
-      "error_desc": "AppId or secret is not right.",
+      "error_code": 100000,
+      "error_desc": "token error.",
     };
   }
+
+}
+
+const proxy = async (ctx) => {
+  let options = {
+    "rejectUnauthorized": false,
+    resolveWithFullResponse: true,
+    method: ctx.method,
+    uri: 'https://117.78.60.188:8743' + ctx.url,
+    qs:ctx.query,
+    json: true,
+  };
+  if(ctx.request.type == "application/x-www-form-urlencoded"){
+    options.form = ctx.request.body;
+  }else{
+    options.body = ctx.request.body;
+  }
+  const response = await rp(options);
+  ctx.body = response.body;
+  ctx.status = response.statusCode;
+}
+
+
+// router.use('/api', authRequired);
+router.use('/api', authRequiredProxy);
+
+// 1.1.1 Authentication
+// router.post('/iocm/app/sec/v1.1.0/login', async (ctx) => {
+//   let appId = ctx.request.body.appId;
+//   let secret = ctx.request.body.secret;
+//   if ((appId == "test") && (secret == "test")) {
+//     ctx.body = {
+//       "accessToken": "950a7cc9-5a8a-42c9-a693-40e817b1a4b0",
+//       "tokenType": "bearer",
+//       "expiresIn": 7200,
+//       "refreshToken": "773a0fcd-6023-45f8-8848-e141296cb3cb",
+//       "scope": "default",
+//     };
+//   } else {
+//     ctx.status = 401;
+//     ctx.body = {
+//       "error_code": 100208,
+//       "error_desc": "AppId or secret is not right.",
+//     };
+//   }
+// })
+router.post('/iocm/app/sec/v1.1.0/login', async (ctx) => {
+  await proxy(ctx);
 })
 
 // 1.1.2 Token Refreshing
+// router.post('/iocm/app/sec/v1.1.0/refreshToken', async (ctx) => {
+//   let token = ctx.request.body;
+//   if (token.appId == "test" && token.secret == "test") {
+//     if (token.refreshToken == "773a0fcd-6023-45f8-8848-e141296cb3cb") {
+//       ctx.body = {
+//         "accessToken": "56465b41-429d-436c-ad8d-613d476ff322",
+//         "tokenType": "bearer",
+//         "expiresIn": 7200,
+//         "refreshToken": "773a0fcd-6023-45f8-8848-e141296cb3cb",
+//         "scope": "default",
+//       };
+//     } else {
+//       ctx.status = 401;
+//       ctx.body = {
+//         "error_code": 100006,
+//         "error_desc": "Refresh access token failed.",
+//       };
+//     }
+//   } else {
+//     ctx.status = 401;
+//     ctx.body = {
+//       "error_code": 100208,
+//       "error_desc": "AppId or secret is not right.",
+//     };
+//   }
+// })
 router.post('/iocm/app/sec/v1.1.0/refreshToken', async (ctx) => {
-  let token = ctx.request.body;
-  if (token.appId == "test" && token.secret == "test") {
-    if (token.refreshToken == "773a0fcd-6023-45f8-8848-e141296cb3cb") {
-      ctx.body = {
-        "accessToken": "56465b41-429d-436c-ad8d-613d476ff322",
-        "tokenType": "bearer",
-        "expiresIn": 7200,
-        "refreshToken": "773a0fcd-6023-45f8-8848-e141296cb3cb",
-        "scope": "default",
-      };
-    } else {
-      ctx.status = 401;
-      ctx.body = {
-        "error_code": 100006,
-        "error_desc": "Refresh access token failed.",
-      };
-    }
-  } else {
-    ctx.status = 401;
-    ctx.body = {
-      "error_code": 100208,
-      "error_desc": "AppId or secret is not right.",
-    };
-  }
+  await proxy(ctx);
 })
 
 // 1.2.1 Common Events Subscription
@@ -165,6 +216,128 @@ router.put('/api/vehicle/v1.0/xcalls/:caseId', async (ctx) => {
     ctx.body = {
       "error_code": "100xxx",
       "error_desc": "The xcall is not existed.",
+    };
+  }
+})
+
+// 1.3.3 Query ecall/bcall Reporting 
+router.get('/api/vehicle/v1.0/xcalls', async (ctx) => {
+  let vin = ctx.query.vin;
+  let caseId = ctx.query.caseId;
+  if (vin || caseId) {
+    ctx.body = {
+      "xcallInfos": [
+        {
+          "caseId": caseId ? caseId : "xxxx",
+          "eventType": "eCallReport ",
+          "timestamp": "xxxxxxxxxx",
+          "eventTime": "xxxxxxxxxxxxxx",
+          "vin": vin ? vin : "xxxxxxxxxxxxxx",
+          "data": {
+            "serviceType": "ecall",
+            "messageType": "create",
+            "callCenterName": "DSSP",
+            "callCenterCreator": "Admin",
+            "callCenterUrl": "http://www.huawei.com",
+            "htime": "2017-09-07T08:08:35Z",
+            "otime": "2017-09-07T08:08:38Z",
+            "ptime": "2017-09-07T08:08:39Z",
+            "manual": null,
+            "veh": {
+              "vis": "34567890",
+              "make": "Peugeot",
+              "model": "",
+              "body": "",
+              "color": "",
+              "mdate": "2017-09-14",
+              "ddate": "2017-09-14",
+              "hwver": "v0.0",
+              "swver": "v0.0",
+              "lang": "nglais",
+              "energy": "",
+            },
+            "geo": {
+              "url": "xxx",
+              "head": {
+                "est": 17,
+                "value": 54
+              },
+              "raw": {
+                "x": 1234,
+                "y": 3234,
+                "z": 0,
+                "est": 48
+              },
+              "p1": {
+                "x": 111,
+                "y": 11
+              },
+              "p2": {
+                "x": 222,
+                "y": 22
+              },
+              "p3": {
+                "x": 333,
+                "y": 33
+              },
+              "p4": {
+                "x": 444,
+                "y": 44
+              },
+              "p5": {
+                "x": 555,
+                "y": 55
+              },
+              "p6": {
+                "x": 666,
+                "y": 66
+              },
+              "p7": {
+                "x": 777,
+                "y": 77
+              },
+              "p8": {
+                "x": 888,
+                "y": 88
+              },
+              "p9": {
+                "x": 999,
+                "y": 99
+              },
+              "unit": "WGS84",
+              "sats": 59
+            },
+            "status": {
+              "rolled": null,
+              "airbag": null,
+              "crash": null,
+              "tow": null,
+              "theft": null,
+              "engine": null,
+              "noignition": null,
+              "moving": "",
+              "front": null,
+              "rear": null,
+              "side": null,
+              "inCar": null,
+              "passengers": 3,
+              "deccel": null,
+              "urlPhoto": null
+            },
+            "diag": null,
+            "pfid": "5b24e",
+            "caller": "44444444",
+            "voice": "22855",
+            "ccid": null
+          }
+        }
+      ]
+    };
+  } else {
+    ctx.status = 400;
+    ctx.body = {
+      "error_code": 100022,
+      "error_desc": "The input is invalid.",
     };
   }
 })
@@ -403,27 +576,32 @@ router.get('/api/vehicle/v1.0/vehicles/:vin/elementaryServices', async (ctx) => 
 })
 
 // 1.4.7 Service Activation/Deactivation
-router.put('/api/vehicle/v1.0/vehicles/:vin/elementaryServices', async (ctx) => {
-  let vin = ctx.params.vin;
-  let body = ctx.request.body;
-  if (body.businessServices) {
-    if (vin) {
-      ctx.status = 200;
-      ctx.body = {};
-    } else {
-      ctx.status = 400;
-      ctx.body = {
-        "error_code": "100xxx",
-        "error_desc": "The VIN is not existed.",
-      };
-    }
-  } else {
-    ctx.status = 400;
-    ctx.body = {
-      "error_code": 100022,
-      "error_desc": "The input is invalid.",
-    };
-  }
+// router.put('/api/vehicle/v1.0/vehicles/elementaryServices', async (ctx) => {
+//   let body = ctx.request.body.root;
+//   let vin = body.vin;
+//   let requestId = body.requestId;
+//   if (body.businessServices) {
+//     if (vin) {
+//       requestId = requestId ? requestId : uuidv4();
+//       ctx.status = 200;
+//       ctx.body = {requestId: requestId };
+//     } else {
+//       ctx.status = 400;
+//       ctx.body = {
+//         "error_code": "100xxx",
+//         "error_desc": "The VIN is not existed.",
+//       };
+//     }
+//   } else {
+//     ctx.status = 400;
+//     ctx.body = {
+//       "error_code": 100022,
+//       "error_desc": "The input is invalid.",
+//     };
+//   }
+// })
+router.put('/api/vehicle/v1.0/vehicles/elementaryServices', async (ctx) => {
+  await proxy(ctx);
 })
 
 // 1.5.2 Vehicle Basic Information Query
@@ -442,13 +620,21 @@ router.get('/api/vehicle/v1.0/vehicles/:vin/genericConfigurations', async (ctx) 
           "uin": "0C000356541039000084",
           "deviceType": "ATB",
           "swVersion": "xxxx",
-          "hwVersion": "xxxx"
+          "hwVersion": "xxxx",
+          "brand":"xxxx",
+          "name":"xxxx",
+          "atbType":"xxxx",
+          "remark":"xxxx",
         },
         {
           "uin": "0C000356541039000085",
           "deviceType": "NAC",
           "swVersion": "xxxx",
-          "hwVersion": "xxxx"
+          "hwVersion": "xxxx",
+          "brand":"xxxx",
+          "name":"xxxx",
+          "atbType":"xxxx",
+          "remark":"xxxx",
         }
       ]
     };
@@ -824,9 +1010,9 @@ router.get('/api/vehicle/v1.0/vehicles/:vin/analysisDatas/reports/ecoDrivings', 
   };
 })
 
-const logger = (ctx, next) => {
+const logger = async (ctx, next) => {
   console.log(`${(new Date()).toJSON()} ${ctx.request.method} ${ctx.request.url}`);
-  next();
+  await next();
 }
 
 
